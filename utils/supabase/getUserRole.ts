118bde6731a28e835ce5d628f893f1dfbbb56530
harikiran138 <155863015+harikiran138@users.noreply.github.com>
2025-12-21 13:04:59 +0530

    refactor: use verifySession in getUserRole

--- utils/supabase/getUserRole.ts
+++ utils/supabase/getUserRole.ts
@@ -1,21 +1,11 @@
-import { jwtDecode } from 'jwt-decode'
-
-import { createClient as ServerClient } from './server'
+import { verifySession } from '@/lib/session'
 import { Tables } from './supabase'
 
 export async function getServerUserRole () {
-    const supabase = ServerClient()
-    const userData = await supabase.auth.getSession()
-
-    if (userData.error) {
-        console.log('Error getting user data', userData.error)
-        supabase.auth.signOut()
+    const session = await verifySession()
+    
+    if (session?.role) {
+        return session.role as Tables<'user_roles'>['role']
     }
-
-    if (userData.data?.session?.access_token) {
-        const decodedToken = jwtDecode(userData.data.session.access_token)
-        // @ts-expect-error
-        const userRole = decodedToken?.user_role
-        return userRole as Tables<'user_roles'>['role']
-    }
+    return undefined
 }
